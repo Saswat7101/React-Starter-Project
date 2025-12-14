62cd6c4005e9db990e44de434a74ef3aea41b329
Saswat <mohantysaswat154@gmail.com>
2025-12-14 12:26:17 +0530

    Adding click listeners to buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import { CORE_CONCEPTS } from "./data.js";
 import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept.jsx";
+import TabButton from "./components/TabButton.jsx";
 
 function Main() {
+  function handleSelect(selectedButton) {
+    console.log(selectedButton);
+  }
   return (
     <main>
       <section id="core-concepts">
@@ -13,6 +17,17 @@ function Main() {
           <CoreConcept {...CORE_CONCEPTS[2]} />
           <CoreConcept {...CORE_CONCEPTS[3]} />
         </ul>
+      </section>
+      <section id="examples">
+        <h2>Examples</h2>
+        <menu>
+          <TabButton onSelect={() => handleSelect("components")}>
+            Components
+          </TabButton>
+          <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
+          <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
+          <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
+        </menu>
       </section>
     </main>
   );
